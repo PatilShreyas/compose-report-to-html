3c1b7f63ec74a67776259cfee18a6343f6971adc
Shreyas <shreyaspatilg@gmail.com>
2023-02-04 15:10:43 +0530

    Configure group and description for task and refactor around output path

--- plugin/src/main/kotlin/dev/shreyaspatil/composeCompilerMetricsGenerator/plugin/task/ComposeCompilerReportGenerateTask.kt
+++ plugin/src/main/kotlin/dev/shreyaspatil/composeCompilerMetricsGenerator/plugin/task/ComposeCompilerReportGenerateTask.kt
@@ -3,15 +3,15 @@ package dev.shreyaspatil.composeCompilerMetricsGenerator.plugin.task
 import com.android.build.api.variant.Variant
 import dev.shreyaspatil.composeCompilerMetricsGenerator.core.ComposeCompilerMetricsProvider
 import dev.shreyaspatil.composeCompilerMetricsGenerator.core.ComposeCompilerRawReportProvider
-import dev.shreyaspatil.composeCompilerMetricsGenerator.core.utils.ensureDirectory
 import dev.shreyaspatil.composeCompilerMetricsGenerator.generator.HtmlReportGenerator
 import dev.shreyaspatil.composeCompilerMetricsGenerator.generator.ReportSpec
 import dev.shreyaspatil.composeCompilerMetricsGenerator.plugin.ComposeCompilerReportExtension
-import java.io.File
+import java.io.FileNotFoundException
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.tasks.GradleBuild
 import org.gradle.configurationcache.extensions.capitalized
+import java.io.File
 
 const val KEY_ENABLE_REPORT_GEN = "dev.shreyaspatil.composeCompiler.reportGen.enable"
 
@@ -19,26 +19,33 @@ fun Project.configureComposeCompilerReportGenTaskForVariant(
     variant: Variant,
     reportExtension: ComposeCompilerReportExtension
 ) {
-    tasks.register(
-        variant.name + "ComposeCompilerHtmlReport",
-        GradleBuild::class.java
-    ) {
+    val taskName = variant.name + "ComposeCompilerHtmlReport"
+    tasks.register(taskName, GradleBuild::class.java) {
         startParameter.projectProperties[KEY_ENABLE_REPORT_GEN] = true.toString()
         tasks = listOf("compile${variant.name.capitalized()}Kotlin")
 
         doFirst {
-            cleanupDirectory(reportExtension.outputDirectory.get())
+            cleanupDirectory(reportExtension.outputPath.get())
         }
 
         doLast {
             generateReport(reportExtension)
         }
+
+        group = "compose compiler report"
+        description = "Generate Compose Compiler Metrics and Report"
     }
 }
 
-fun cleanupDirectory(directory: File) {
-    ensureDirectory(directory) { "'${directory}' is not a directory" }
-    directory.deleteRecursively()
+fun cleanupDirectory(directory: String) {
+    val dirFile = File(directory)
+    if (dirFile.exists()) {
+        if (!dirFile.isDirectory) {
+            throw FileNotFoundException("'${directory}' is not a directory")
+        }
+    }
+
+    dirFile.deleteRecursively()
 }
 
 /**
@@ -49,7 +56,7 @@ fun Project.executingComposeCompilerReportGenerationGradleTask() = runCatching {
 }.getOrNull() == "true"
 
 private fun Task.generateReport(reportExtension: ComposeCompilerReportExtension) {
-    val outputDirectory = reportExtension.outputDirectory.get()
+    val outputDirectory = reportExtension.outputPath.get().let { File(it) }
 
     // Create a report specification with application name
     val reportSpec = ReportSpec(reportExtension.name.get())
