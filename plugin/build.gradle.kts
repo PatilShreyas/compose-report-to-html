38bd019bb3e53183b943cf8642aaff2e1f184d2b
Shreyas <shreyaspatilg@gmail.com>
2023-02-04 16:23:25 +0530

    Configure plugin publishing

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,12 @@
+val GROUP: String by project
+val POM_NAME: String by project
+val VERSION_NAME: String by project
+val POM_DESCRIPTION: String by project
+
 plugins {
     `kotlin-dsl`
-    id("java-gradle-plugin")
-    id(libs.plugins.mavenPublish.get().pluginId)
+    `java-gradle-plugin`
+    alias(libs.plugins.gradle.plugin.publish)
 }
 
 repositories {
@@ -10,6 +15,9 @@ repositories {
     gradlePluginPortal()
     maven(url = "https://plugins.gradle.org/m2/")
 }
+
+group = GROUP
+version = VERSION_NAME
 
 dependencies {
     implementation(gradleApi())
@@ -22,4 +30,18 @@ dependencies {
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
+}
+
+gradlePlugin {
+    website.set("https://github.com/PatilShreyas/compose-report-to-html")
+    vcsUrl.set("https://github.com/PatilShreyas/compose-report-to-html")
+    plugins {
+        create("reportGenPlugin") {
+            id = "dev.shreyaspatil.compose-compiler-report-generator"
+            displayName = POM_NAME
+            description = POM_DESCRIPTION
+            implementationClass = "dev.shreyaspatil.composeCompilerMetricsGenerator.plugin.ReportPlugin"
+            tags.set(listOf("android", "compose", "report", "jetpackcompose", "composecompiler"))
+        }
+    }
 }
